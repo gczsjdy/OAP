b95d9fae25c7a0d20275440ab5776f9367ca1646
GuoChenzhao <chenzhao.guo@intel.com>
2018-05-15 14:40:43 +0800

    Fix

---
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/io/ParquetDataFileMeta.scala
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.datasources.oap.io
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FSDataInputStream, Path}
+import org.apache.hadoop.util.StringUtils
+import org.apache.parquet.format.converter.ParquetMetadataConverter._
+import org.apache.parquet.hadoop.ParquetFileReader
+import org.apache.parquet.hadoop.metadata.ParquetMetadata
+
+private[oap] class ParquetDataFileMeta(val footer: ParquetMetadata) extends DataFileMeta {
+
+  require(footer != null, "footer of ParquetDataFileMeta should not be null.")
+
+  def this(conf: Configuration, path: String) {
+    this(ParquetFileReader.readFooter(conf, new Path(StringUtils.unEscapeString(path)), NO_FILTER))
+  }
+
+  override def fin: FSDataInputStream = null
+
+  override def len: Long = 0
+
+  override def getGroupCount: Int = footer.getBlocks.size()
+
+  override def getFieldCount: Int =
+    footer.getFileMetaData.getSchema.getColumns.size()
+
+}
